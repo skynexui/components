04d6c10431de33d2130f5f75feea9112a65776b6
Mario Souto <soutomarios@gmail.com>
2021-09-02 07:50:46 -0300

    fix: Box component

--- src/components/Box/web/index.tsx
+++ src/components/Box/web/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled from 'styled-components';
 import withStyledInternalProps from '../../../theme/utils/withStyledInternalProps';
 import { BoxPropsBase, Styles } from '../styles';
