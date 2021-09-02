cb45fc27ceb1959048c72abfe7fd7a8c036116a5
Mario Souto <soutomarios@gmail.com>
2021-09-02 07:02:19 -0300

    feat: add example monorepo (#6)
    * feat: add monorepo example with expo, nextjs and storybook
    * update first potential working version
    * finish the example allowing us to import code from a project inside the monorepo

---
+++ examples/with-monorepo-storybook-nextjs-expo/packages/common-ui/screens/HomeScreen.js
@@ -0,0 +1,8 @@
+import Text from '@skynexui/native/components/Text';
+
+export default function HomeScreen() {
+  return (
+    // For common code, prefer always use native imports and convert it inside web projects through webpack aliases
+    <Text>This is my dream comming true! I can use the same API with React everywhere! "Learn once and write everywhere"</Text>
+  )
+}
---
+++ examples/with-monorepo-storybook-nextjs-expo/packages/with-nextjs/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HomeScreen from 'common-ui/screens/HomeScreen';
+
+function HomePage() {
+  return <HomeScreen />
+}
+
+export default HomePage
