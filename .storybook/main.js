650603e55cf26f82654b74783e061f40dcbf4861
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2021-12-12 15:59:36 -0300

    fix: add support to storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/react"
+}
