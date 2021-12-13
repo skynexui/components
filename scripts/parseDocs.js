47d4b622eb8b033335d12fe3718efa4bff4846ca
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2021-12-13 19:57:53 -0300

    feat: add support to export docs

---
+++ scripts/parseDocs.js
@@ -0,0 +1,32 @@
+const { readdirSync, readFileSync, writeFileSync } = require('fs');
+const pkg = require('../package.json');
+// ============================================================================
+
+
+const files = readdirSync('./dist')
+  .filter((filePath) => filePath.endsWith('.mdx'))
+  .map((filePath) => {
+    const fileContentDirty = readFileSync(`./dist/${filePath}`, 'utf8');
+    let lock = true;
+    const fileLines = [];
+
+    fileContentDirty
+      .split('\n')
+      .forEach((line) => {
+        if (line.includes('@lib/components')) {
+          fileLines.push(line.replace('@lib/components', pkg.name))
+          fileLines.push('\n');
+        };
+        if (line.startsWith('# ')) lock = false;
+        if (lock) return;
+        fileLines.push(line); 
+      });
+
+    return {
+      filePath,
+      fileContent: fileLines.join('\n'),
+    };
+  })
+  .forEach(({ filePath, fileContent }) => {
+    writeFileSync(`./dist/${filePath}`, fileContent, 'utf8');
+  });
