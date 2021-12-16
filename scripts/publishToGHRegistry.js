57e3e7b1065007f86131885aa800a861dc10b2ae
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2021-12-16 11:08:00 -0300

    fix: script to sync with gh registry

---
+++ scripts/publishToGHRegistry.js
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fetchModule = import('node-fetch');
+const { execSync } = require('child_process');
+
+Promise.allSettled([fetchModule]).then(async ([{ value: fetch }]) => {
+  const lastTag = execSync('git describe --abbrev=0 --tags')
+    .toString()
+    .replace('\n', '')
+    .replace('v', '')
+    .trim();
+
+  const response = await fetch
+    .default('https://github.com/skynexui/core/packages/1146437/versions')
+    .then((res) => res.text());
+
+  if (!response.includes(lastTag)) {
+    execSync(
+      'npm publish --@skynexui:registry=https://npm.pkg.github.com --force'
+    );
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(
+    `Up to date with latest version [${lastTag}], nothing to publish`
+  );
+});
