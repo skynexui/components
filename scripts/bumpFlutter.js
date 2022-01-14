f7ab18a589971950202e7a2dddefc457857b5f1d
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-01-14 18:51:55 -0300

    fix: add flutter pubspec bump

---
+++ scripts/bumpFlutter.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const pkg = require('../package.json');
+console.log(`[Bump Flutter to: ${pkg.version}]`);
+
+const pubspecUpdated = fs.readFileSync('./pubspec.yaml', { encoding: 'utf-8' })
+  .split('\n')
+  .map((line) => {
+    if(line.startsWith('version')) return `version: ${pkg.version}`;
+    return line;
+  })
+  .join('\n');
+
+fs.writeFileSync('./pubspec.yaml', pubspecUpdated, { encoding: 'utf-8' });
