46a26ecbb504d93b96cbcff1f0cb78306bbef32d
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-01-17 01:24:37 -0300

    force release

--- examples/demo_base/next.config.js
+++ examples/demo_base/next.config.js
@@ -2,15 +2,19 @@
 /* eslint-disable no-param-reassign */
 const path = require('path');
 
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+
 module.exports = {
   webpack: (config) => {
-    config.resolve.alias.react = path.resolve(
-      __dirname,
-      '..',
-      '..',
-      'node_modules',
-      'react'
-    );
+    if (IS_DEVELOPMENT) {
+      config.resolve.alias.react = path.resolve(
+        __dirname,
+        '..',
+        '..',
+        'node_modules',
+        'react'
+      );
+    }
 
     return config;
   },
