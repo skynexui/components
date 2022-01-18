f2635592eac7b71088349f30c544daffd8d201d6
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-01-17 23:52:22 -0300

    fix: try again

--- scripts/pub_login.sh
+++ scripts/pub_login.sh
@@ -22,9 +22,11 @@ if [ -z "${PUB_DEV_PUBLISH_EXPIRATION}" ]; then
   echo "Missing PUB_DEV_PUBLISH_EXPIRATION environment variable"
   exit 1
 fi
+ 
+echo "$PUB_CACHE/credentials.json"
 
 # Create credentials.json file.
-cat <<EOF > ~/.pub-cache/credentials.json
+cat <<EOF > "$PUB_CACHE/credentials.json"
 {
   "accessToken":"${PUB_DEV_PUBLISH_ACCESS_TOKEN}",
   "refreshToken":"${PUB_DEV_PUBLISH_REFRESH_TOKEN}",
