27320dd72e380f93f8f19b600f9c03407a87c65b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-01-23 02:49:37 -0300

    wip

--- scripts/coder/coder.js
+++ scripts/coder/coder.js
@@ -3,6 +3,9 @@
 console.log('[Coder] running...');
 
 const stylesheetFile = require('./files/stylesheet');
+const themeFile = require('./files/theme');
 
+themeFile.generators.Theme_colors();
+themeFile.generators.Theme_breakpoints();
 stylesheetFile.generators.StyleSheet_attributes();
 stylesheetFile.generators.StyleSheet_constructor();
---
+++ scripts/coder/files/theme.js
@@ -0,0 +1,46 @@
+const core = require('../core/core');
+const path = './lib/core/theme';
+
+const theme = {
+  breakpoints: require('../../../lib/core/theme/breakpoints/breakpoints.json').breakpoints,
+  colors: require('../../../lib/core/theme/colors/colors.json').colors,
+}
+
+const generators = {
+  Theme_breakpoints() {
+    const contract = {
+      name: 'Theme_breakpoints',
+      contract: theme.breakpoints,
+    };
+
+    core.applyContract(contract, `${path}/theme.ts`, ([colorName, value]) => {
+      return `${colorName.replace('Breakpoints.', '')}: ${value},`;
+    });
+
+    core.applyContract(contract, `${path}/theme.dart`, ([colorName, value]) => {
+      return `"${colorName}": ${value},`;
+    });
+  },
+  Theme_colors() {
+    const contract = {
+      name: 'Theme_colors',
+      contract: theme.colors,
+    };
+
+    core.applyContract(contract, `${path}/theme.ts`, ([colorName, value]) => {
+      return `${colorName}: {
+        ${Object.entries(value).map(([colorTone, color]) => `${colorTone}: '${color}',`).join('\n')}
+      },`;
+    });
+
+    core.applyContract(contract, `${path}/theme.dart`, ([colorName, value]) => {
+      return `"${colorName}": {
+        ${Object.entries(value).map(([colorTone, color]) => `"${colorTone}": "${color}",`).join('\n')}
+      },`;
+    });
+  }
+}
+
+module.exports = {
+  generators,
+};
